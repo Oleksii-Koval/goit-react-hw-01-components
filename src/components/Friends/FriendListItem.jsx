b92b804364b83ce0fb6dbd537f787e69487f157e
Oleksii-Koval <oleksii270@gmail.com>
2023-11-30 23:00:42 +0100

    tasks1-3

---
+++ src/components/Friends/FriendListItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function FriendListItem({ id, avatar, name, isOnline }) {
+  return (
+    <li className="item" key={id}>
+      <span className="status">{isOnline}</span>
+      <img className="avatar" src={avatar} alt="User avatar" width="48" />
+      <p className="name">{name}</p>
+    </li>
+  );
+}
+
+export default FriendListItem;
